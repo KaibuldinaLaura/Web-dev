59dfec7f0336981191b93ffded6c982dfdf51dc0
KaibuldinaLaura <kaibuldinalaura@gmail.com>
2020-02-17 22:46:58 +0600

    w4

---
+++ w4/src file of hw4/app/products.ts
@@ -0,0 +1,72 @@
+export const products = [
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1.xESmvuSBuNkHFqDq6xfhVXat.jpg_220x220q90.jpg_.webp',
+    name: 'Beautiful Peony flowers for home/wedding/store/party decoration',
+    price: 2.25,
+    description: '30cm Rose Pink Silk Peony Artificial Flowers Bouquet 5 Big Head and 4 Bud Cheap Fake Flowers for Home Wedding Decoration indoor',
+    rating: 4.8
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1Jb6ECf5TBuNjSspcq6znGFXae.jpg_220x220q90.jpg_.webp',
+    name: 'Peony Home Decor Bouquet',
+    price: 1.56,
+    description: 'Peony DIY Party Decoration Vintage Silk Artificial Flowers Small Rose Wedding Fake Flowers Festival Supplies Home Decor Bouquet',
+    rating: 4.6
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1TutabUGF3KVjSZFmq6zqPXXaG/Funny-Cartoon-Phone-Case-for-iPhone-XS-XS-Max-XR-Cute-Cat-Pattern-Soft-Silicone-Case.jpg_220x220xz.jpg_.webp',
+    name: 'Cases for iPhone',
+    price: 0.99,
+    description: 'Funny Cartoon Phone Case for iPhone 5/6/7/8',
+    rating: 4.8
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/Haa51576b6ea24a7fa1e24b4299dc3ce1m/Prumya-Stylish-Cute-Cactus-Letters-Phone-Case-For-iphone-11-XS-MAX-XR-X-8-7.jpg_220x220xz.jpg_.webp',
+    name: 'Cute Cases for iPhone, Samsung',
+    price: 2.46,
+    description: 'Prumya Stylish Cute Cactus Letters Cases for iPhone Samsung',
+    rating: 4.9
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/H5c3420e30d4e4cc281655c78edc3a2989/Rick-And-Morty-Black-TPU-case-for-iPhone-XR-XS-Max-X-Coque-For-iPhone-11.jpg_220x220xz.jpg_.webp',
+    name: 'Case for Iphone',
+    price: 1.59,
+    description: 'Rick And Morty Black TPU case for iPhone',
+    rating: 4.8
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1tspuhtknBKNjSZKPq6x6OFXaG/Apple-iPhone-7-Plus-3GB-RAM-32-128GB-256GB-ROM-IOS-Quad-Core-12-0MP-Camera.jpg_220x220xz.jpg_.webp',
+    name: 'iPhone 7',
+    price: 85.88,
+    description: 'Apple iPhone 7 Plus 3GB 32gb Nfc ',
+    rating: 3.8
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1Jb6ECf5TBuNjSspcq6znGFXae.jpg_220x220q90.jpg_.webp',
+    name: 'Peony Home Decor Bouquet',
+    price: 1.56,
+    description: 'Peony DIY Party Decoration Vintage Silk Artificial Flowers Small Rose Wedding Fake Flowers Festival Supplies Home Decor Bouquet',
+    rating: 4.6
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1tspuhtknBKNjSZKPq6x6OFXaG/Apple-iPhone-7-Plus-3GB-RAM-32-128GB-256GB-ROM-IOS-Quad-Core-12-0MP-Camera.jpg_220x220xz.jpg_.webp',
+    name: 'iPhone 7',
+    price: 79.59,
+    description: 'Apple iPhone 7 Plus 3GB 32gb Nfc ',
+    rating: 4.2
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1Jb6ECf5TBuNjSspcq6znGFXae.jpg_220x220q90.jpg_.webp',
+    name: 'Peony Home Decor Bouquet',
+    price: 1.56,
+    description: 'Peony DIY Party Decoration Vintage Silk Artificial Flowers Small Rose Wedding Fake Flowers Festival Supplies Home Decor Bouquet',
+    rating: 4.6
+  },
+  {
+    image: 'https://ae01.alicdn.com/kf/HTB1Jb6ECf5TBuNjSspcq6znGFXae.jpg_220x220q90.jpg_.webp',
+    name: 'Peony Home Decor Bouquet',
+    price: 1.56,
+    description: 'Peony DIY Party Decoration Vintage Silk Artificial Flowers Small Rose Wedding Fake Flowers Festival Supplies Home Decor Bouquet',
+    rating: 4.6
+  }
+];
